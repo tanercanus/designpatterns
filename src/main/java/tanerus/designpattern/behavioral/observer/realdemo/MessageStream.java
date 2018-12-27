3a4395d324243de0757274209abed5a950a926fc
Taner Can US <tanercanus@gmail.com>
2018-12-28 01:08:49 +0300

    observer pattern

---
+++ src/main/java/tanerus/designpattern/behavioral/observer/realdemo/MessageStream.java
@@ -0,0 +1,22 @@
+package tanerus.designpattern.behavioral.observer.realdemo;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class MessageStream extends Subject{
+
+	private Deque<String> messageHistory = new ArrayDeque<>();
+	
+	@Override
+	void setState(String message) {
+		messageHistory.add(message);
+		this.notifyObservers();
+		
+	}
+
+	@Override
+	String getState() {
+		return messageHistory.getLast();
+	}
+
+}
---
+++ src/main/java/tanerus/designpattern/behavioral/observer/realdemo/Observer.java
@@ -0,0 +1,6 @@
+package tanerus.designpattern.behavioral.observer.realdemo;
+
+public abstract class Observer {
+	protected Subject subject;
+	abstract void update();
+}
