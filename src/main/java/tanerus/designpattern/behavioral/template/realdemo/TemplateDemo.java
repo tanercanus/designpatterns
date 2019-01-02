0b537aed95861b09e6136c096656b16a463cb423
Taner Can US <tanercanus@gmail.com>
2019-01-02 22:28:48 +0300

    template pattern

---
+++ src/main/java/tanerus/designpattern/behavioral/template/realdemo/TemplateDemo.java
@@ -0,0 +1,19 @@
+package tanerus.designpattern.behavioral.template.realdemo;
+
+public class TemplateDemo {
+
+	public static void main(String[] args) {
+
+		System.out.println("Web Order:");
+
+		OrderTemplate webOrder = new WebOrder();
+		webOrder.processOrder();
+
+		System.out.println("\nStore Order:");
+
+		OrderTemplate storeOrder = new StoreOrder();
+		storeOrder.processOrder();
+
+	}
+
+}
