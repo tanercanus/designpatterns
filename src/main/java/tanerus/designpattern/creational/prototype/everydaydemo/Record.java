5ca4d5ca7f6ad829ae13236c7b9228a15f7df50a
lenovo <lenovo@DESKTOP-5G2KBLS>
2019-01-04 15:37:50 +0300

    prototype pattern

---
+++ src/main/java/tanerus/designpattern/creational/prototype/everydaydemo/Record.java
@@ -0,0 +1,5 @@
+package tanerus.designpattern.creational.prototype.everydaydemo;
+
+public class Record {
+
+}
---
+++ src/main/java/tanerus/designpattern/creational/prototype/realdemo/Movie.java
@@ -0,0 +1,14 @@
+package tanerus.designpattern.creational.prototype.realdemo;
+
+public class Movie extends Item {
+	private String runtime;
+
+	public String getRuntime() {
+		return runtime;
+	}
+
+	public void setRuntime(String runtime) {
+		this.runtime = runtime;
+	}
+
+}
