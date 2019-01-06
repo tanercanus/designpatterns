3a0a784dfc510e13243f6c88b5b9cc283bc72066
Taner Can US <tanercanus@gmail.com>
2019-01-06 22:04:15 +0300

    decorator pattern

---
+++ src/main/java/tanerus/designpattern/structural/decorator/realdemo/SandwichDecorator.java
@@ -0,0 +1,15 @@
+package tanerus.designpattern.structural.decorator.realdemo;
+
+//Decorator
+public abstract class SandwichDecorator implements Sandwich {
+	protected Sandwich customSandwich;
+	
+	public SandwichDecorator(Sandwich customSandwich) {
+		this.customSandwich = customSandwich;
+	}
+	
+	public String make() {
+		return customSandwich.make();
+	}
+	
+}
